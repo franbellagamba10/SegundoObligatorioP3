d921c3db29dee9e1cd247921f821a826da4f775b
Fran <FB262790@fi365.ort.edu.uy>
2022-04-18 22:56:47 -0300

    test de ejecucion en program

--- Ejecutable/Program.cs
+++ Ejecutable/Program.cs
@@ -11,18 +11,40 @@ namespace Ejecutable
 
 
             RepositorioTiposPlantaADO repoTipos = new RepositorioTiposPlantaADO();
+            RepositorioUsuariosADO repoUsuarios = new RepositorioUsuariosADO();
+            RepositorioFrecuenciaRiegoADO repoFR = new RepositorioFrecuenciaRiegoADO();
+            RepositorioTipoIluminacionADO repoTI = new RepositorioTipoIluminacionADO();
+            RepositorioFichasADO repoFichas = new RepositorioFichasADO(repoFR, repoTI);
+            RepositorioPlantasADO repoPlantas = new RepositorioPlantasADO(repoTipos, repoFichas, repoUsuarios);
 
-            TipoPlanta tipo = new TipoPlanta()
+            //TipoPlanta tipo = new TipoPlanta()
+            //{
+            //    nombre = "una plantita",
+            //    descripcion = "es roja y crece dos metros",
+            //};
+            //repoTipos.Create(tipo);
+
+
+            //Usuario user = new Usuario()
+            //{
+            //    email = "otroMail@gmail.com",
+            //    contrasenia = "12345abc",
+            //};
+            //repoUsuarios.Create(user);
+
+            Planta faso = new Planta()
             {
-                nombre = "una plantita",
-                descripcion = "es roja y crece dos metros",
+                tipo = repoTipos.FindById(1),
+                nombreCientifico = "ayahuasca",
+                nombresVulgares = "la que bajo las torres gemelas",
+                descripcion = "404",
+                ambiente = "donde puedas",
+                alturaMaxima = 300,
+                foto = "una_Foto2.jpg",
+                precio = 300,
+                ingresadoPor = repoUsuarios.FindById(1),
             };
-
-            repoTipos.Create(tipo);
-
-
-
-
+            repoPlantas.Create(faso);
         }
     }
 }
