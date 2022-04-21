74f9ad6a42a067ac6ab2a1dc5041aa847a04a4a7
Fran <FB262790@fi365.ort.edu.uy>
2022-04-20 23:16:50 -0300

    feat: Implementa Validar en TipoIluminacion

--- Dominio/Entidades/TipoIluminacion.cs
+++ Dominio/Entidades/TipoIluminacion.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Dominio.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Dominio.Entidades
 {
-    public class TipoIluminacion
+    public class TipoIluminacion : IValidate
     {
         public int id { get; set; }
         public string iluminacion { get; set; }
+
+        public bool Validar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
