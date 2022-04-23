54d805a06fa241e517dbaef291c7b4d227a0ba6c
Fran <FB262790@fi365.ort.edu.uy>
2022-04-22 22:47:27 -0300

    Agrega Login y Logout a controller de Usuarios

--- ProyectoWeb/Controllers/UsuariosController.cs
+++ ProyectoWeb/Controllers/UsuariosController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dominio.Entidades;
+using Fachada;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,10 +11,30 @@ namespace ProyectoWeb.Controllers
 {
     public class UsuariosController : Controller
     {
+        public IManejadorUsuarios manejadorUsuarios { get; set; }
         public IActionResult Index()
         {
             return View();
         }
-    }
-    //otro comentario
+        [HttpPost]
+        public ActionResult Login(string email, string contrasenia)
+        {
+            Usuario user = null;
+            user = manejadorUsuarios.BuscarUsuarioPorSuEmail(email);
+            if (user != null && user.activo && user.contrasenia == contrasenia)
+            {
+                HttpContext.Session.SetInt32("userId", user.id);
+                return Redirect("Plantas/Index");
+            }
+            //mensaje de error al loguearse?
+            return Redirect("Login/Index");
+        }
+
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return Redirect("Login/Index");
+        }
+
+    }   
 }
