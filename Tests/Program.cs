237af6c94e363a4143ee4228d7954cbf2e5e1b98
Fran <FB262790@fi365.ort.edu.uy>
2022-04-22 23:07:32 -0300

    Agrega proyecto de testing

---
+++ Tests/Program.cs
@@ -0,0 +1,22 @@
+﻿using Dominio.Entidades;
+using System;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Pruebo el correcto: ");
+            Console.WriteLine(Usuario.ValidarEmail("unmail@algo.com"));
+            Console.WriteLine(Usuario.ValidarEmail("=========================================="));
+            Console.WriteLine("Pruebo el incorrecto: ");
+            Console.WriteLine(Usuario.ValidarEmail("@algo.com"));
+            Console.WriteLine(Usuario.ValidarEmail("unmail@algocom"));
+            Console.WriteLine(Usuario.ValidarEmail("unmail@"));
+            Console.WriteLine(Usuario.ValidarEmail("un@mail@algo.com"));
+        }
+    }
+}
