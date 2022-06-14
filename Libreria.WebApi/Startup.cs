3a06e974e10c2bda0ed5924b2001586a999cd3ac
Fran <FB262790@fi365.ort.edu.uy>
2022-06-13 21:56:27 -0300

    feat: agrega proyecto WebApi, nugets, WebApi Controller de planta y configuracion de StartUp

---
+++ Libreria.WebApi/Startup.cs
@@ -0,0 +1,60 @@
+using Datos;
+using Dominio.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+//using Libreria.Dominio.InterfacesRepositorios;
+//using AccesoDatos.EF;
+
+namespace Libreria.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddNewtonsoftJson(options => 
+                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            string strCon = Configuration.GetConnectionString("ConexionApi");
+            services.AddDbContext<ViveroContext>(options => options.UseSqlServer(strCon));
+
+            services.AddScoped<IRepositorioPlantas,RepositorioPlantasEF>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
